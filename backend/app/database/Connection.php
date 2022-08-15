facfed57fb837e23f7d3b8306250c62b9fddb63e
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-08-15 08:53:26 -0300

    First commit

---
+++ backend/app/database/Connection.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\database;
+
+use PDO;
+
+class Connection
+{
+  public function connect(){
+    return new PDO("mysql:host=localhost;dbname=phpJWT","root","",[
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ]);
+  }
+}
